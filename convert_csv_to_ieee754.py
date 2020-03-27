f95830538dfcbd31ae542fe45d4e6a387d0074c6
7m4mon <7m4mon@gmail.com>
2020-03-27 23:59:10 +0900

    specify a file name as an argument.

--- convert_csv_to_ieee754.py
+++ convert_csv_to_ieee754.py
@@ -1,21 +1,32 @@
 import csv
 import struct
+import sys
 
-def main():
+def csv2float(in_file_name = './input.csv' , out_file_name = './output.flt', row_val = 1):
     bin_ary = bytearray([])
-    with open('./input.csv') as f_in:
+    with open(in_file_name) as f_in:
         reader = csv.reader(f_in)
         for row in reader:
             try:
-                v = float(row[1])   # non comma is possible with index zero.
+                v = float(row[row_val])   # non comma is possible with index zero.
             except ValueError:
                 print('ValueError')
             else:
                 bin_ary.extend(struct.pack('<f', v)) # little endian, 32bit float : compatible for GRC File Sink Block
                 # print(hex(struct.unpack('<L', struct.pack('<f', v))[0])) # debug
                 
-    with open('./output.flt', 'wb') as f_out:
+    with open(out_file_name, 'wb') as f_out:
         f_out.write(bin_ary)
         print('done')
         
-main()
+if __name__ == '__main__':
+    args = sys.argv
+    if 4 <= len(args):
+        csv2float(args[1], args[2], int(args[3]))
+    elif 3 == len(args):
+        csv2float(args[1], args[2])
+    elif 2 == len(args):
+        csv2float(args[1])
+    else:
+        print('input_file, output_file, row')
+        
